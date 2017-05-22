7b1cb5cc4b9711fa427af29d1895d3816858bf25
jprice <jprice@izeni.com>
2017-05-22 15:44:04 -0600

    Removes video and video compilations.  Adds Album and Artist models.  Refactors views to account for albums and artists.

--- apps/music/admin.py
+++ apps/music/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from . import models
 
 
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre',)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'genre',)
+
+
 class MusicAdmin(admin.ModelAdmin):
-    list_display = ('title', 'artist', 'album', 'year', 'disc', 'track',)
-    ordering = ('uploaded_at',)
+    list_display = ('title', 'album', 'disc_num', 'track_num',)
 
 
 class CompilationAdmin(admin.ModelAdmin):
@@ -12,7 +19,7 @@ class CompilationAdmin(admin.ModelAdmin):
     ordering = ('created',)
 
 
-admin.site.register(models.Video, MusicAdmin)
+admin.site.register(models.Artist, ArtistAdmin)
+admin.site.register(models.Album, AlbumAdmin)
 admin.site.register(models.Audio, MusicAdmin)
 admin.site.register(models.AudioCompilation, CompilationAdmin)
-admin.site.register(models.VideoCompilation, CompilationAdmin)
--- apps/music/models.py
+++ apps/music/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
 from easy_thumbnails.fields import ThumbnailerImageField
+from precise_bbcode.fields import BBCodeTextField
 
-from apps.common.models import UploadedAtMixin, CreatedModifiedMixin, UUIDPrimaryKey
-
-
-def video_upload_to(instance, filename):
-    ext = (filename.split('.')[-1]).lower()
-    filename = '{}.{}'.format(instance.slug, ext)
-    return '/'.join(['music', 'videos', filename])
+from apps.common.models import (UploadedAtMixin, CreatedModifiedMixin,
+                                UUIDPrimaryKey)
 
 
 def audio_upload_to(instance, filename):
@@ -20,59 +16,84 @@ def audio_upload_to(instance, filename):
 def audio_compilation_upload_to(instance, filename):
     ext = (filename.split('.')[-1]).lower()
     filename = '{}.{}'.format(instance.slug, ext)
-    return '/'.join(['music', 'compilations', 'audio', filename])
+    return '/'.join(['music', 'compilations', filename])
+
+
+def artist_image_upload_to(instance, filename):
+    ext = (filename.split('.')[-1]).lower()
+    filename = '{}.{}'.format(instance.slug, ext)
+    return '/'.join(['music', 'artists', filename])
+
+
+def album_cover_upload_to(instance, filename):
+    ext = (filename.split('.')[-1]).lower()
+    filename = '{}.{}'.format(instance.slug, ext)
+    return '/'.join(['music', 'album', filename])
+
+
+class Artist(UUIDPrimaryKey):
+    slug = models.SlugField(
+        max_length=256, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=256, blank=False, null=False)
+    genre = models.CharField(max_length=256)
+    bio = BBCodeTextField(max_length=5096, blank=True, null=True)
+    image = ThumbnailerImageField(
+        upload_to=artist_image_upload_to, blank=True, null=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(UUIDPrimaryKey):
+    slug = models.SlugField(max_length=256, blank=False, null=False, unique=True)
+    artist = models.ForeignKey('Artist',
+        blank=False, null=False, related_name='albums')
+    title = models.CharField(max_length=256, blank=False, null=False)
+    year = models.DateField()
+    cover_art = ThumbnailerImageField(
+        upload_to=album_cover_upload_to, blank=True, null=True)
+    genre = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ('artist', 'title', 'year',)
+
+    def __str__(self):
+        return '{}: {}'.format(self.artist.name, self.title)
 
 
 class TrackInformationMixin(models.Model):
+    slug = models.SlugField(max_length=256, blank=False, null=False, unique=True)
     title = models.CharField(max_length=256, blank=False, null=False)
-    artist = models.CharField(max_length=256, blank=False, null=False)
-    album = models.CharField(max_length=256, blank=False, null=False)
-    genre = models.CharField(max_length=256)
-    year = models.DateField()
-    disc = models.IntegerField()
-    track = models.IntegerField()
+    album = models.ForeignKey('Album',
+        blank=True, null=True, related_name='tracks')
+    disc_num = models.IntegerField()
+    track_num = models.IntegerField()
 
     class Meta:
         abstract = True
 
 
-class Video(UUIDPrimaryKey, UploadedAtMixin, TrackInformationMixin):
-    slug = models.SlugField(max_length=256, blank=False, null=False)
-    file = models.FileField(upload_to=video_upload_to, blank=False, null=False)
-    thumbnail = ThumbnailerImageField(
-        upload_to=video_upload_to, blank=True, null=True)
-
-    def __str__(self):
-        return '{} - {}'.format(self.uploaded_at, self.title)
-
-
 class Audio(UUIDPrimaryKey, UploadedAtMixin, TrackInformationMixin):
-    slug = models.SlugField(max_length=256, blank=False, null=False)
     file = models.FileField(upload_to=audio_upload_to, blank=False, null=False)
-    thumbnail = ThumbnailerImageField(
-        upload_to=audio_upload_to, blank=True, null=True)
+
+    class Meta:
+        ordering = (
+            'album__artist__name', 'album__title', 'disc_num', 'track_num', 'title',)
 
     def __str__(self):
-        return '{} - {}'.format(self.uploaded_at, self.title)
+        return '{} - {} - {}'.format(self.title,
+            self.album.artist.name, self.album.title)
 
 
 class AudioCompilation(UUIDPrimaryKey, CreatedModifiedMixin):
     title = models.CharField(max_length=256, blank=False, null=False)
     slug = models.SlugField(max_length=256, blank=False, null=False)
     thumbnail = ThumbnailerImageField(
-        upload_to=audio_upload_to, blank=True, null=True)
+        upload_to=audio_compilation_upload_to, blank=True, null=True)
     tracks = models.ManyToManyField(Audio)
-
-    def __str__(self):
-        return '{} - {}'.format(self.created, self.title)
-
-
-class VideoCompilation(UUIDPrimaryKey, CreatedModifiedMixin):
-    title = models.CharField(max_length=256, blank=False, null=False)
-    slug = models.SlugField(max_length=256, blank=False, null=False)
-    thumbnail = ThumbnailerImageField(
-        upload_to=audio_upload_to, blank=True, null=True)
-    tracks = models.ManyToManyField(Video)
 
     def __str__(self):
         return '{} - {}'.format(self.created, self.title)
