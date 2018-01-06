2d07d8b5f8e417b3934c697d007ee8de635128e0
jprice <jprice@izeni.com>
2018-01-05 17:39:39 -0700

    Finishes graphql schema that roughly resembles the REST api functionality.

--- apps/api/schema.py
+++ apps/api/schema.py
@@ -1,8 +1,7 @@
-from graphene import relay, List
+from graphene import List
 from graphene_django.types import DjangoObjectType
-# from graphene_django.filter import DjangoFilterConnectionField
-from .models import (EmailUser, UserRank, Category, Subcategory, Thread, Post,
-    Conversation, Message, Report, Shout, Notification,)
+from .models import (EmailUser, Category, Subcategory, Thread, Post,
+    Conversation, Message, Shout,)
 
 
 class EmailUserNode(DjangoObjectType):
@@ -23,10 +22,44 @@ class SubcategoryNode(DjangoObjectType):
         exclude_fields = ('auth_req',)
 
 
+class ThreadNode(DjangoObjectType):
+    # TODO: Subcategory filter
+    class Meta:
+        model = Thread
+
+
+class PostNode(DjangoObjectType):
+    # TODO: Thread filter
+    class Meta:
+        model = Post
+
+
+class ConversationNode(DjangoObjectType):
+    # TODO: Must be authenticated
+    class Meta:
+        model = Conversation
+
+
+class MessageNode(DjangoObjectType):
+    # TODO: Must be authenticated
+    class Meta:
+        model = Message
+
+
+class ShoutNode(DjangoObjectType):
+    class Meta:
+        model = Shout
+
+
 class Query(object):
     all_users = List(EmailUserNode)
     all_categories = List(CategoryNode)
     all_subcategories = List(SubcategoryNode)
+    all_threads = List(ThreadNode)
+    all_posts = List(PostNode)
+    all_conversations = List(ConversationNode)
+    all_messsages = List(MessageNode)
+    all_shouts = List(ShoutNode)
 
     def resolve_all_users(self, info, **kwargs):
         qs = EmailUser.objects.order_by('username')
@@ -49,5 +82,55 @@ class Query(object):
         if user.is_authenticated() and user.is_banned:
             return Subcategory.objects.none()
         if not user.is_authenticated():
-            qs = qs.filter(auth_req=False)
+            qs = qs.filter(auth_req=False, parent__auth_req=False)
+        return qs.all()
+
+    def resolve_all_threads(self, info, **kwargs):
+        qs = Thread.objects.order_by('-created')
+        user = info.context.user
+
+        if user.is_authenticated() and user.is_banned:
+            return Thread.objects.none()
+        if not user.is_authenticated():
+            qs = qs.filter(
+                category__auth_req=False,
+                category__parent__auth_req=False)
+        return qs.all()
+
+    def resolve_all_posts(self, info, **kwargs):
+        qs = Post.objects.order_by('-created')
+        user = info.context.user
+
+        if user.is_authenticated() and user.is_banned:
+            return Post.objects.none()
+        if not user.is_authenticated():
+            qs = qs.filter(
+                thread__category__auth_req=False,
+                thread__category__parent__auth_req=False)
+        return qs.all()
+
+    def resolve_all_conversations(self, info, **kwargs):
+        qs = Conversation.objects.all()
+        user = info.context.user
+
+        if user.is_authenticated() and user.is_banned:
+            return Conversation.objects.none()
+        qs = qs.filter(users__in=[user])
+        return qs.all()
+
+    def resolve_all_messages(self, info, **kwargs):
+        qs = Message.objects.all()
+        user = info.context.user
+
+        if user.is_authenticated() and user.is_banned:
+            return Message.objects.none()
+        qs = qs.filter(conversation__users__in=[user])
+        return qs.all()
+
+    def resolve_all_shouts(self, info, **kwargs):
+        qs = Shout.objects.all()[:25]
+        user = info.context.user
+
+        if user.is_authenticated() and user.is_banned:
+            return Shout.objects.none()
         return qs.all()
