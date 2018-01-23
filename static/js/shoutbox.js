d3fd0b82c8463323066c0453386ac12c42cab955
jprice <jprice@izeni.com>
2018-01-22 21:33:28 -0700

    Fix editor text colors

--- static/js/shoutbox.js
+++ static/js/shoutbox.js
@@ -50,7 +50,7 @@ $(function() {
     var editor = $('#shoutEditor').sceditor({
       plugins: 'bbcode',
       toolbar: 'bold,italic,underline,strike|font,size,color,link|date,time|source,removeformat',
-      style: '/static/scss/_editor.min.css',
+      style: '/static/scss/_editor.css',
       fonts: 'Arial,Arial Black,Comic Sans MS,Courier New,Georgia,Impact,Sans-serif,Serif,Storybook,Times New Roman,Trebuchet MS,Truckin,Verdana',
       autoExpand: true,
       emoticonsEnabled: false,
