764f9257aa837127581ec0912cb378d30177ef5a
shakedown-street <pricejordan@outlook.com>
2018-04-05 15:41:48 -0600

    Version tick

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="punkweb_boards",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     include_package_data=True,
     description="Django forum boards with bbcode support.",
