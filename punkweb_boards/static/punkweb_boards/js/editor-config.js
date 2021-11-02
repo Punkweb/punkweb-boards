14f4263460e031a4ddf558dc7b5213b8d331ab1d
jprice <jprice@izeni.com>
2021-11-02 15:52:41 -0600

    Remove winterland and storybook fonts, add default favicon

--- punkweb_boards/static/punkweb_boards/js/editor-config.js
+++ punkweb_boards/static/punkweb_boards/js/editor-config.js
@@ -4,7 +4,7 @@ $(function() {
       format: 'bbcode',
       toolbar: 'bold,italic,underline,strike|bulletlist,orderedlist,center,horizontalrule|font,size,color,quote,code,link,image|date,time|source,maximize,removeformat',
       style: '/static/punkweb_boards/scss/editor.css',
-      fonts: 'Arial,Arial Black,Comic Sans MS,Inconsolata,Courier New,Georgia,Impact,Open Sans,Montserrat,Sans-serif,Serif,Storybook,Times New Roman,Trebuchet MS,Verdana,Winterland',
+      fonts: 'Arial,Arial Black,Comic Sans MS,Inconsolata,Courier New,Georgia,Impact,Open Sans,Montserrat,Sans-serif,Serif,Times New Roman,Trebuchet MS,Verdana',
       autoExpand: true,
       emoticonsEnabled: false,
       emoticonsCompat: true,
