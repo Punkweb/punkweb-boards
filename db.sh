10909fa175a629fe65a1eafd4101da9ad9b24830
jprice <jprice@izeni.com>
2022-02-24 12:54:41 -0700

    Remove db and dockerless-run bash files

--- db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE punkweb_boards; \
-CREATE USER punkweb_boards WITH PASSWORD 'punkweb_boards'; \
-ALTER ROLE punkweb_boards SET client_encoding TO 'utf8'; \
-ALTER ROLE punkweb_boards SET timezone TO 'UTC'; \
-GRANT ALL PRIVILEGES ON DATABASE punkweb_boards TO punkweb_boards;" | sudo su postgres -c psql
--- dockerless-run.sh
+++
@@ -1,5 +0,0 @@
-export DATABASE_HOST=localhost
-
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
